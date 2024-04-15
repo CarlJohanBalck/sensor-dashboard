76184daf3cd311fe1c047f56eed74e3bc26ce94b
Carl Johan Balck <carljohanbalck@gmail.com>
2024-04-15 13:59:23 +0200

    first commit

---
+++ src/component/LineChart.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from 'react';
+import Chart, { ChartConfiguration, ChartOptions } from 'chart.js/auto';
+
+interface LineChartProps {
+  data: number[];
+  labels: string[];
+}
+
+const LineChart: React.FC<LineChartProps> = ({ data, labels }) => {
+  const chartContainer = useRef<HTMLCanvasElement>(null);
+  const chartInstance = useRef<Chart>();
+
+  useEffect(() => {
+    if (chartContainer.current) {
+      // Ensure the previous chart instance is destroyed
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+      }
+      const ctx = chartContainer.current.getContext('2d');
+      if (ctx) {
+        const config: ChartConfiguration<'line'> = {
+          type: 'line',
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                label: 'Line Chart',
+                data: data,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.4,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        };
+
+        const options: ChartOptions<'line'> = config.options || {};
+
+        chartInstance.current = new Chart(ctx, config);
+
+        return () => {
+          // Cleanup: Destroy the chart instance before unmounting
+          if (chartInstance.current) {
+            chartInstance.current.destroy();
+          }
+        };
+      }
+    }
+  }, [data, labels]);
+
+  return <canvas ref={chartContainer} />;
+};
+
+export default LineChart;
---
+++ src/component/TemperatureChart.tsx
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from 'react';
+import LineChart from './LineChart';
+import axios from 'axios';
+import { stat } from 'fs';
+
+const TemperatureChart: React.FC = () => {
+
+interface SensorDataEntry {
+    date: string;
+    humidity: number;
+    pressure: number;
+    temperature: number;
+    }
+
+const [sensorData, setSensorData] = useState<SensorDataEntry[]>([]);
+
+  useEffect(() => {
+    fetchIngredients()
+  }, [])
+  async function fetchIngredients() {
+    try {
+      await axios.get('http://192.168.0.120:5001/daily-sensor-data').then(response => {
+        setSensorData(response.data)
+       
+      })
+      .catch(error => {
+        console.error('Error fetching stats:', error);
+      });
+  } catch (error) {
+
+      throw error;
+    }
+  }
+
+  let temp: number[] = []
+  let hum: number[] = []
+  let press: number[] = []
+  let date: string[] = []
+
+  for (let i = 0; i < sensorData.length; i++) {
+    const data = sensorData[i]
+    temp.push(data.temperature)
+    hum.push(data.humidity)
+    press.push(data.pressure)
+    date.push(data.date)
+  } 
+
+  const tempData = temp;
+  const humData = hum;
+  const pressData = press;
+  const tempLabels = date;
+
+
+  return (
+    <div>
+      <h1>Temperature</h1>
+      <LineChart data={tempData} labels={tempLabels} />
+      <h1>Humidity</h1>
+      <LineChart data={humData} labels={tempLabels} />
+      <h1>Pressure</h1>
+      <LineChart data={pressData} labels={tempLabels} />
+    </div>
+  );
+};
+
+export default TemperatureChart;
