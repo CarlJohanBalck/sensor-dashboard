b67dfbd939776c7cbb6c7da287e37e8ac222aefa
Carl Johan Balck <carljohanbalck@gmail.com>
2024-04-15 14:29:58 +0200

    updated imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TemperatureChart from './component/TemperatureChart';
+import TemperatureChart from './components/TemperatureChart';
 
 
 function App() {
